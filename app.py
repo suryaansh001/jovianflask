3687e11b0a2f0c9aa57d0210b6be75910210fef5
Sin_taX <141883923+suryaansh001@users.noreply.github.com>
2024-03-10 08:53:46 +0000

    fnctional and aesthetic improvements

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'salary': 'Rs.10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Flask Developer',
+        'salary': 'Rs.15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'salary': 'Rs.20,00,000'
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', jobs=JOBS)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
